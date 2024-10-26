624f915a8fb0517d36feb6881e13a949052ac228
Raushan Raj <137853785+raushan2104@users.noreply.github.com>
2024-10-27 01:26:35 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/DashboardStats';
- import NoticePage from './pages/NoticePage';
-import ExamPage from './pages/ExamPage';
-import SummaryPage from './components/SummaryPage';
+
+
 import AcademicPerformance from './components/marks&academic'
 import AttendanceManagement from './pages/AttendanceManagement'
  import ExamAssignmentSchedule from './components/ExamAssignmentSchedule';
@@ -20,9 +19,9 @@ import ReplyPage from './Replypage';
             <div className="flex-grow p-4">  
                 <Routes>
                     <Route path="/" element={<Dashboard />}/>
-                     <Route path="/notices" element={<NoticePage />} />
-                    <Route path="/exam" element={<ExamPage />} />
-                    <Route path="/summary" element={<SummaryPage />} />
+                     
+                    
+                    
                     <Route path="/marks&academic" element={< AcademicPerformance/>} />
                     <Route path="/attendance" element={< AttendanceManagement/>} />
                     <Route path="/Schedules" element={< ExamAssignmentSchedule/>} />
