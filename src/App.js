0b20d2ac2db4a5a8d7fc3920d3f255686e58d783
Raushan Raj <137853785+raushan2104@users.noreply.github.com>
2024-10-27 03:04:10 +0530

    fixed firebase error

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
                   <Dashboard />
                 </ProtectedRoute>
               } 
-            />
+            />+
             <Route 
               path="/marks&academic" 
               element={
