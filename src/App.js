cc9a7787a91061d4f2c6db1a555ea747830fdcd3
Gamerking177 <rajaksh03@gmail.com>
2024-10-26 18:02:45 +0530

    title name changed

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import NoticePage from './pages/NoticePage';
 import ExamPage from './pages/ExamPage';
 import AttendancePage from './components/AttendancePage';
 import SummaryPage from './components/SummaryPage';
+import ForgetPassword from '../src/auth/forget password/forgetpass'
+import Login from '../src/auth/login/login'
    const App = () => (
     <Router>
         <div className="flex">
             <Sidebar />
             <div className="flex-grow p-4">  
                 <Routes>
-                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/" element={<Dashboard />} Component={Login}/>
                     <Route path="/students" element={<StudentDashboard />} />
                     <Route path="/notices" element={<NoticePage />} />
                     <Route path="/exam" element={<ExamPage />} />
                     <Route path="/attendance" element={<AttendancePage />} />
                     <Route path="/summary" element={<SummaryPage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/forgetpassword" element={<ForgetPassword />} />
 
                 </Routes>
             </div>
