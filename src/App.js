1431275c95f794d94c0051227dfecaefdd5e54d8
Gamerking177 <rajaksh03@gmail.com>
2024-10-26 22:27:03 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../src/firebase/firebase'; // import your Firebase auth instance here
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/DashboardStats';
-import StudentDashboard from './components/StudentCard';  
+import StudentDashboard from './components/StudentCard';
 import NoticePage from './pages/NoticePage';
 import ExamPage from './pages/ExamPage';
-
 import SummaryPage from './components/SummaryPage';
-import AcademicPerformance from './components/marks&academic'
-import AttendanceManagement from './pages/AttendanceManagement'
-   const App = () => (
-    <Router>
-        <div className="flex">
-            <Sidebar />
-            <div className="flex-grow p-4">  
-                <Routes>
-                    <Route path="/" element={<Dashboard />}/>
-                    <Route path="/students" element={<StudentDashboard />} />
-                    <Route path="/notices" element={<NoticePage />} />
-                    <Route path="/exam" element={<ExamPage />} />
-                    
-                    <Route path="/summary" element={<SummaryPage />} />
-                    <Route path="/marks&academic" element={< AcademicPerformance/>} />
-                    <Route path="/attendance" element={< AttendanceManagement/>} />
-                </Routes>
+import AcademicPerformance from './components/marks&academic';
+import AttendanceManagement from './pages/AttendanceManagement';
+import Login from '../src/auth/login/login'; // Add a login component for unauthenticated users
+
+const App = () => {
+    const [user, loading] = useAuthState(auth);
+
+    if (loading) {
+        return <p>Loading...</p>; // Optionally add a loading spinner here
+    }
+
+    return (
+        <Router>
+            <div className="flex">
+                {user && <Sidebar />} {/* Show Sidebar only if user is logged in */}
+                <div className="flex-grow p-4">
+                    <Routes>
+                        {user ? (
+                            <>
+                                <Route path="/" element={<Dashboard />} />
+                                <Route path="/students" element={<StudentDashboard />} />
+                                <Route path="/notices" element={<NoticePage />} />
+                                <Route path="/exam" element={<ExamPage />} />
+                                <Route path="/summary" element={<SummaryPage />} />
+                                <Route path="/marks&academic" element={<AcademicPerformance />} />
+                                <Route path="/attendance" element={<AttendanceManagement />} />
+                            </>
+                        ) : (
+                            <>
+                                <Route path="/login" element={<Login />} />
+                                <Route path="*" element={<Navigate to="/login" />} />
+                            </>
+                        )}
+                    </Routes>
+                </div>
             </div>
-        </div>
-    </Router>
-);
+        </Router>
+    );
+};
 
 export default App;
